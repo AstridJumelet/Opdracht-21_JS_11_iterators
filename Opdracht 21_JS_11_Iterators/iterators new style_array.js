602c710dc1124ed721e95fea1cf4422187d1da78
Astrid Jumelet <89905425+AstridJumelet@users.noreply.github.com>
2021-11-16 17:39:46 +0100

    Add files via upload

---
+++ Opdracht 21_JS_11_Iterators/iterators new style_array.js
@@ -0,0 +1,3 @@
+console.log("** iterators NEW style:array methods **");
+const array1SelectedColors3 = ["yellow", "blue", "red", "orange"];
+array1SelectedColors3.forEach(element => console.log(element));
---
+++ Opdracht 21_JS_11_Iterators/iterators old style_loops.js
@@ -0,0 +1,14 @@
+console.log("** iterators OLD style: while loop **");
+
+let selectedColorsArray = ["yellow", "blue", "red", "orange"];
+let i=0;
+while (i < selectedColorsArray.length) {
+    console.log(selectedColorsArray[i]);
+    i++;
+}
+
+console.log("** iterators old style: for loop**");
+let selectedColors1Array = ["yellow", "blue", "red", "orange"];
+for (i = 0; i<selectedColors1Array.length; i++) {
+    console.log(selectedColors1Array[i]);
+}
